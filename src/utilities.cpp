4ba57a423ef5f83e9c49f900d680bb0a8c474a1c
Dmitriy Goldberg <dmgoldberg@edu.hse.ru>
2026-01-23 19:58:08 +0300

    tech commit

--- src/utilities.cpp
+++
@@ -1,8 +0,0 @@
-#include "utilities.h"
-
-#include <regex>
-
-bool isInteger(const std::string& str) {
-    std::regex integer_regex("^[+-]?\\d+$");
-    return std::regex_match(str, integer_regex);
-}
--- src/utilities.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-bool isInteger(const std::string& str);
